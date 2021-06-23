c112bd8df46da6a21fe44c58e74abb626a8e9caf
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-06-23 16:11:14 +1200

    partial test to download jobs

--- src/BrowseJobs.Tests/JobsTests.cs
+++ src/BrowseJobs.Tests/JobsTests.cs
@@ -5,29 +5,35 @@ namespace BrowseJobs.Tests
 {
     public class JobsTests
     {
-        private Mock<IService> _service;
+        private Mock<IService> _serviceMock;
 
         [SetUp]
         public void Setup()
         {
-            _service = new Mock<IService>();
+            _serviceMock = new Mock<IService>();
         }
 
         [Test]
         public void ShouldGetJobs()
         {
-            new Jobs(_service.Object);
+            _serviceMock.Setup(x => x.Download());
+
+            new Jobs(_serviceMock.Object);
+
+            _serviceMock.Verify(x => x.Download(), Times.Once);
         }
     }
 
-    public interface IService {}
+    public interface IService {
+        void Download();
+    }
 
     public class Jobs {
 
-        private readonly IService service;
+        // private readonly IService service;
         public Jobs(IService service)
         {
-            
+            service.Download();
         }
     }
 }
