196a5e762363cbc73ec2b1e21f02377ef07ff06e
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-06-24 10:37:36 +1200

    display jobs

--- src/BrowseJobs.Tests/JobsTests.cs
+++ src/BrowseJobs.Tests/JobsTests.cs
@@ -6,15 +6,17 @@ namespace BrowseJobs.Tests
     public class JobsTests
     {
         private Mock<IService> _serviceMock;
+        private Mock<IResult> _resultMock;
 
         [SetUp]
         public void Setup()
         {
             _serviceMock = new Mock<IService>();
+            _resultMock = new Mock<IResult>();
         }
 
         [Test]
-        public void ShouldGetJobs()
+        public void ShouldDownloadJobs()
         {
             _serviceMock.Setup(x => x.Download());
 
@@ -22,18 +24,38 @@ namespace BrowseJobs.Tests
 
             _serviceMock.Verify(x => x.Download(), Times.Once);
         }
+
+        [Test]
+        public void ShouldDisplayJobs() {
+            _resultMock.Setup(x => x.Display());
+            _serviceMock.Setup(x => x.Download()).Returns(_resultMock.Object);
+
+            new Jobs(_serviceMock.Object).Display();
+
+            _resultMock.Verify(x => x.Display(), Times.Once);
+        }
     }
 
     public interface IService {
-        void Download();
+        IResult Download();
+    }
+
+    public interface IResult {
+        void Display();
     }
 
     public class Jobs {
 
-        // private readonly IService service;
+        private readonly IService service;
+        private readonly IResult result;
         public Jobs(IService service)
         {
-            service.Download();
+            this.service = service;
+            this.result = service.Download();
+        }
+
+        public void Display() {
+            this.result.Display();
         }
     }
 }
